867ee1f8023c52ecd030cdf22813950a9469c041
u3577271 <80959628+u3577271@users.noreply.github.com>
2021-04-25 16:52:55 +0800

    Update check_connection.cpp

--- check_connection.cpp
+++ check_connection.cpp
@@ -5,16 +5,16 @@ void check(int **area, int x, int y,int ** record, int ** count, int a){        
 										//given the area recording the position of mines, the x,y coordinate of the block that the user choose, the array recording the clicking records and the parametre of the area           
 	record[x][y] = 1;
 	
-	if((x-1) >= 0 && (x-1) < a && area[x-1][y] == 0 && record[x-1][y] == 0 && count[x][y] == 0){		
+	if((x-1) >= 0 && (x-1) < a && area[x-1][y] == 0 && record[x-1][y] == 0 && count[x][y] == 0){	//check if the block left to the (x,y) block has mines	
 		check(area, x-1, y, record, count, a);
 	}
-	if((x+1) >= 0 && (x+1) < a && area[x+1][y] == 0 && record[x+1][y] == 0 && count[x][y] == 0){
+	if((x+1) >= 0 && (x+1) < a && area[x+1][y] == 0 && record[x+1][y] == 0 && count[x][y] == 0){	//check if the block right to the (x,y) block has mines
 		check(area, x+1, y, record, count, a);
 	}
-	if((y-1) >= 0 && (y-1) < a && area[x][y-1] == 0 && record[x][y-1] == 0 && count[x][y] == 0){
+	if((y-1) >= 0 && (y-1) < a && area[x][y-1] == 0 && record[x][y-1] == 0 && count[x][y] == 0){	//check if the block above the (x,y) block has mines
 		check(area, x, y-1, record, count, a);
 	}
-	if((y+1) >= 0 && (y+1) < a && area[x][y+1] == 0 && record[x][y+1] == 0 && count[x][y] == 0){
+	if((y+1) >= 0 && (y+1) < a && area[x][y+1] == 0 && record[x][y+1] == 0 && count[x][y] == 0){	//check if the block below the (x,y) block has mines
 		check(area, x, y+1, record, count, a);
 	}
 	
